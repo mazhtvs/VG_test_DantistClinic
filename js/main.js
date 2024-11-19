1537628ff5683d16e1e4ba994a0986c1d0dbd67e
mazhi <slava.mazhitov@mail.ru>
2024-11-19 17:45:36 +0500

    2

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true, 
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true
+                }
+            }
+        ]
+    });
+});
+
+
+$(document).ready(function () {
+    $('.vertical-slider').slick({
+        vertical: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true, 
+        prevArrow: '<button class="slick-prev"></button>',
+        nextArrow: '<button class="slick-next"></button>',
+        infinite: false,
+        dots: false,
+        speed: 500
+    });
+});
+
+$(document).ready(function () {
+    if ($(window).width() <= 768) {
+        $('.slider_mobile').slick({
+            dots: true,
+            arrows: true,
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 300,
+        });
+    }
+});
